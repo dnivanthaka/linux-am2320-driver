0173250eb62ffd7e9ea302fe17ad638ccf554eb4
dnivanthaka <nivanthaka@gmail.com>
2016-07-16 21:56:28 +0530

    Updated the working version, Updated the README

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # linux-am2320-driver
 Linux Device Driver for AM2302
+
+**Cleaning the directory**
+make clean
+
+**Building**
+make
+
+**Inserting into kernel**
+sudo insmod am2320.ko
+then do,
+echo am2320 0x5c > /sys/class/i2c-adapter/i2c-1/new_device
+
+**To remove from kernel**
+echo 0x5c > /sys/class/i2c-adapter/i2c-1/delete_device
+then do,
+sudo rmmod am2320
+
+**Viewing measurements**
+cat /sys/bus/i2c/devices/1-005c/temp1_input
+cat /sys/bus/i2c/devices/1-005c/humidity1_input
+OR
+/sys/class/i2c-adapter/i2c-1/1-005c/temp1_input
+/sys/class/i2c-adapter/i2c-1/1-005c/humidity1_input
--- am2320.c
+++ am2320.c
@@ -16,9 +16,15 @@
 #include <linux/device.h>
 #include <linux/delay.h>
 #include <linux/interrupt.h>
+#include <linux/jiffies.h>
+#include <linux/hwmon.h>
+#include <linux/hwmon-sysfs.h>
+#include <linux/sysfs.h>
 
 #define AM2320_NAME "am2320"
 #define AM2320_I2C_ADDRESS 0x5C
+
+#define AM2320_UPDATE_INTERVAL_MILLIES 1000
 
 #define AM2320_I2C_HIGH_HUMIDITY    0x00
 #define AM2320_I2C_LOW_HUMIDITY     0x01
@@ -40,12 +46,76 @@ struct am2320_device {
                    // On recieve
                   // sensor responds with Function code, Num bytes, Reg. 1, Reg. 2, Reg. 3, Reg. 4
                   // CRC 2 bytes
+    unsigned long last_updated;     //In jiffies
+    unsigned int update_interval;   //In milliseconds
+    u8 valid;                       //Validity of data
+    struct attribute_group *group;
 };
 
 struct am2320_data {
     s16 tempterature;
     s16 humidity;
 };
+//------------- Prototypes ----------------------------//
+static int am2320_i2c_read_data(struct i2c_client *client, u8 func, u8 reg, u8 len);
+static void am2320_update_data(struct i2c_client *client, struct am2320_data *data);
+//-----------------------------------------------------//
+
+//----------- Sensor sysfs ---------------------------------------------------//
+static ssize_t humidity_input(struct device *dev, struct device_attribute *attr, 
+        char *buf){
+    struct i2c_client  *client  = to_i2c_client(dev);
+    //struct am2320_device *am2320 = i2c_get_clientdata(client);
+    struct am2320_data readings;
+    am2320_update_data(client, &readings);
+
+    return sprintf(buf, "%d\n", readings.humidity);
+}
+static ssize_t temp_input(struct device *dev, struct device_attribute *attr, 
+        char *buf){
+    struct i2c_client  *client  = to_i2c_client(dev);
+    //struct am2320_device *am2320 = i2c_get_clientdata(client);
+    struct am2320_data readings;
+    am2320_update_data(client, &readings);
+
+    return sprintf(buf, "%d\n", readings.tempterature);
+}
+
+static ssize_t temp_max(struct device *dev, struct device_attribute *attr, 
+        char *buf){
+    //struct i2c_client  *client  = to_i2c_client(dev);
+    //struct am2320_device *am2320 = i2c_get_clientdata(client);
+
+    return sprintf(buf, "100\n");
+}
+
+static ssize_t temp_min(struct device *dev, struct device_attribute *attr, 
+        char *buf){
+    //struct i2c_client  *client  = to_i2c_client(dev);
+    //struct am2320_device *am2320 = i2c_get_clientdata(client);
+
+    //return snprintf(buf, PAGE_SIZE - 1, "0\n");
+    return sprintf(buf, "0\n");
+}
+
+static SENSOR_DEVICE_ATTR(temp1_max, S_IWUSR | S_IRUGO, temp_max, NULL,  100);
+static SENSOR_DEVICE_ATTR(temp1_min, S_IWUSR | S_IRUGO, temp_min, NULL,  0);
+static SENSOR_DEVICE_ATTR(temp1_input, S_IWUSR | S_IRUGO, temp_input, NULL,  0);
+static SENSOR_DEVICE_ATTR(humidity1_input, S_IWUSR | S_IRUGO, humidity_input, NULL,  0);
+
+//static struct attribute *am2320_attrs[] = {
+static struct device_attribute *am2320_attrs[] = {
+    &sensor_dev_attr_temp1_max.dev_attr.attr,
+    &sensor_dev_attr_temp1_min.dev_attr.attr,
+    &sensor_dev_attr_temp1_input.dev_attr.attr,
+    &sensor_dev_attr_humidity1_input.dev_attr.attr,
+    NULL
+};
+
+static const struct attribute_group am2320_group = {
+    .attrs = am2320_attrs,
+};
+//------------------------------------------------------------------------------//
 
 static int am2320_wakeup(struct i2c_client *client){
     i2c_smbus_read_byte_data(client, AM2320_I2C_HIGH_HUMIDITY);
@@ -95,28 +165,47 @@ static int am2320_i2c_read_data(struct i2c_client *client, u8 func, u8 reg, u8 l
         goto exit_lock;
     }
 
+    //am2320->last_updated = jiffies;
+    //am2320->valid = 1;
+
     mutex_unlock(&am2320->lock);
     //Do a CRC here
+
+    return 0;
     
 exit_lock:
-    //mutex_unlock(&am2320->lock);
+    mutex_unlock(&am2320->lock);
     return ret;
     
-    
-    return 0;
 }
 
+static void am2320_update_data(struct i2c_client *client, struct am2320_data *data){
+    struct am2320_device *am2320;
+    
+    am2320 = i2c_get_clientdata(client);
+    if(am2320_i2c_read_data(client, AM2320_I2C_FUNC_READ, AM2320_I2C_HIGH_HUMIDITY, 4) == 0){
+        data->humidity     = (am2320->tbuffer[2] << 8) | (am2320->tbuffer[3]);
+        data->tempterature = (am2320->tbuffer[4] << 8) | (am2320->tbuffer[5]);
+
+        am2320->last_updated = jiffies;
+        am2320->valid = 1;
+    }
+}
+/*
 static irqreturn_t am2320_interrupt_thread(int irq, void *data){
     struct am2320_device *am2320 = data;
 
     return IRQ_HANDLED;
 }
-
+*/
 static int am2320_i2c_probe(struct i2c_client *client, 
         const struct i2c_device_id *id){
 
     struct i2c_adapter *adapter = client->adapter;
     struct am2320_device *am2320;
+    //TODO Remove this
+    //struct am2320_data readings;
+    //struct device *hwmon_dev;
     
     int ret;
     int error;
@@ -134,13 +223,34 @@ static int am2320_i2c_probe(struct i2c_client *client,
     }
     
     am2320->client = client;
+    am2320->valid = 0;
+    am2320->update_interval = AM2320_UPDATE_INTERVAL_MILLIES;
+    am2320->last_updated = 0;
+
+    //am2320->group = &am2320_group;
+
     i2c_set_clientdata(client, am2320);
     mutex_init(&am2320->lock);
+
+
+
+    //Registering sysfs files
+    device_create_file(&client->dev,  (const struct device_attribute *)&sensor_dev_attr_temp1_max);
+    device_create_file(&client->dev,  (const struct device_attribute *)&sensor_dev_attr_temp1_min);
+    device_create_file(&client->dev,  (const struct device_attribute *)&sensor_dev_attr_temp1_input);
+    device_create_file(&client->dev,  (const struct device_attribute *)&sensor_dev_attr_humidity1_input);
         
-    am2320_i2c_read_data(client, AM2320_I2C_FUNC_READ, AM2320_I2C_HIGH_HUMIDITY, 4);
+    /*am2320_i2c_read_data(client, AM2320_I2C_FUNC_READ, AM2320_I2C_HIGH_HUMIDITY, 4);
 
-    dev_info(&client->dev, "Read values = %x, %x, %x, %x, %x, %x, %x, %x\n", am2320->tbuffer[0], am2320->tbuffer[1], am2320->tbuffer[2], am2320->tbuffer[3], am2320->tbuffer[4], am2320->tbuffer[5], am2320->tbuffer[6], am2320->tbuffer[7]);
+    dev_info(&client->dev, "Read values = %x, %x, %x, %x, %x, %x, %x, %x\n", am2320->tbuffer[0], am2320->tbuffer[1], am2320->tbuffer[2], am2320->tbuffer[3], am2320->tbuffer[4], am2320->tbuffer[5], am2320->tbuffer[6], am2320->tbuffer[7]);*/
+    //am2320_update_data(client, &readings);
+    //dev_info(&client->dev, "READINGS = H=%d, T=%d\n", readings.humidity, readings.tempterature);
 
+    /*hwmon_dev = hwmon_device_register_with_groups(&client->dev, 
+                                                    client->name, am2320, am2320->group);*/
+
+
+    return 0;
     /*
     error = request_threaded_irq(client->irq, NULL, 
             am2320_interrupt_thread,IRQF_TRIGGER_RISING | IRQF_ONESHOT,
@@ -167,6 +277,7 @@ static int am2320_i2c_remove(struct i2c_client *client){
 }
 
 
+
 static const struct i2c_device_id am2320_id[] = {
     {AM2320_NAME, 0},
     {}
@@ -175,6 +286,7 @@ static const struct i2c_device_id am2320_id[] = {
 MODULE_DEVICE_TABLE(i2c, am2320_id);
 
 static struct i2c_driver am2320_i2c_driver = {
+    //.class = I2C_CLASS_HWMON,
     .driver = {
         .name = AM2320_NAME
     },
